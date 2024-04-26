cadbe375ecb5fe86aa2f642d0a261886711bff7a
Marvin Drees <marvin.drees@9elements.com>
2024-04-26 12:18:50 +0200

    feat: initial commit

---
+++ go.mod
@@ -0,0 +1,19 @@
+module embedded_reflash
+
+go 1.22.1
+
+require (
+	github.com/facebook/openbmc/tools/flashy v0.0.0-20240426005344-8186f9d9d7a9
+	github.com/moby/sys/mountinfo v0.7.1
+	golang.org/x/sys v0.17.0
+)
+
+require (
+	github.com/Jeffail/gabs v1.4.0 // indirect
+	github.com/pierrec/lz4/v4 v4.1.14 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/u-root/u-root v0.14.0 // indirect
+	github.com/u-root/uio v0.0.0-20240209044354-b3d14b93376a // indirect
+	github.com/vtolstov/go-ioctl v0.0.0-20151206205506-6be9cced4810 // indirect
+	golang.org/x/exp v0.0.0-20240222234643-814bf88cf225 // indirect
+)
---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/facebook/openbmc/tools/flashy/lib/flash/flashcp"
+	"github.com/moby/sys/mountinfo"
+	"golang.org/x/sys/unix"
+)
+
+func main() {
+	dev := flag.String("device", "/dev/mtd0", "Flash device to write to")
+	img := flag.String("image", "/tmp/flash.img", "Flash image to write")
+	skipRemount := flag.Bool("skip-remount", false, "Skip remounting rootfs read-only")
+	flag.Parse()
+
+	if os.Geteuid() != 0 {
+		log.Fatal("This program must be run as root!")
+	}
+
+	if !*skipRemount {
+		log.Println("Blocking file system access to flash...")
+		if _, err := os.Stat("/proc/sysrq-trigger"); os.IsNotExist(err) {
+			log.Println("No SysRq support, trying to remount rootfs read-only instead...")
+
+			minfo, err := mountinfo.GetMounts(RWFilter)
+			if err != nil {
+				log.Fatalf("Failed to get mount info: %v", err)
+			}
+
+			for _, i := range minfo {
+				if err := unix.Mount("", i.Mountpoint, "", unix.MS_REMOUNT|unix.MS_RDONLY, ""); err != nil {
+					log.Fatalf("Failed to remount rootfs: %v", err)
+				}
+			}
+		}
+
+		if err := os.WriteFile("/proc/sysrq-trigger", []byte("u"), 00644); err != nil {
+			log.Fatalf("Failed to remount rootfs: %v", err)
+		}
+	}
+
+	log.Println("Overwriting flash contents...")
+	if err := flashcp.FlashCp(*img, *dev, 0); err != nil {
+		log.Fatalf("Failed to overwrite flash: %v", err)
+	}
+
+	log.Println("Resetting system now...")
+	if err := unix.Reboot(unix.LINUX_REBOOT_CMD_RESTART); err != nil {
+		log.Fatalf("Failed to reboot machine: %v", err)
+	}
+}
+
+func RWFilter(info *mountinfo.Info) (bool, bool) {
+	if info.FSType == "tmpfs" || strings.Contains(info.Options, "ro") {
+		return false, false
+	}
+
+	return true, false
+}
